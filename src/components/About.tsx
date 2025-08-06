66e32a6b53f25e116734123799ab8d1013657dfb
FarmaJava <facundomaldonado258@gmail.com>
2025-08-06 11:05:27 -0300

    commit 1.5

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ArrowLeft } from 'lucide-react';
+import icono from '../resource/Icono.png';
 
 interface AboutProps {
   onBack: () => void;
@@ -16,24 +17,23 @@ const About: React.FC<AboutProps> = ({ onBack }) => {
         <ArrowLeft className="h-5 w-5" />
         Volver
       </button>
-
       <div className="bg-white rounded-2xl shadow-lg p-8">
         <h2 className="text-3xl font-bold text-center text-gray-800 mb-6">
-          ¿Quiénes somos?
+          ¿Quiénes somos? Historia
         </h2>
 
         <img
-          src="https://images.unsplash.com/photo-1586773860414-d37222d8fce3"
-          alt="Quiénes somos"
-          className="rounded-xl w-full h-64 object-cover mb-6"
+          src={icono}
+          alt=""
+          className="w-80 h-80 mx-auto mb-4" 
         />
 
-        <p className="text-gray-700 leading-relaxed text-lg text-center">
-          Somos CandyCrushers, una confitería dedicada a ofrecer las mejores
-          delicias artesanales para alegrar tus días. Nos apasiona crear
-          experiencias únicas con nuestros productos, hechos con amor,
-          creatividad y los mejores ingredientes. ¡Gracias por confiar en
-          nosotros!
+        <p className="text-gray-700 leading-relaxed text-lg text-left">
+            Somos dos amigos que, desde chicos, compartimos la pasión por lo dulce. Siempre que había una excusa para cocinar, ahí estábamos, inventando recetas y probando combinaciones.
+            <br />
+            Después de años de amistad y de muchas meriendas improvisadas, decidimos estudiar gastronomía juntos. Durante la carrera, fuimos perfeccionando técnicas, aprendiendo de los mejores y soñando con tener algún día nuestro propio espacio.
+            <br />
+            Así nació CandyCrushers, una confitería online pensada para compartir esa misma emoción con la que empezamos: la de crear algo rico, casero y con mucho amor. Todo lo que ves en esta tienda fue hecho pensando en eso… en endulzar momentos, como lo hicimos siempre entre nosotros.
         </p>
       </div>
     </div>
