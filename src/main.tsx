1cacaecf84bd2f1928e8f65ba41c5a07ab0f8e5b
FarmaJava <facundomaldonado258@gmail.com>
2026-03-03 08:14:14 -0300

    correcciones 2

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { UserProvider } from './context/UserContext'; // 👈 Asegurate que la ruta esté bien
+import { UserProvider } from './context/UserContext'; 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
